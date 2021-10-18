cebcbdf7919e0839f0daec8d825ad4edd3cde270
wxysz <wxysz81@yahoo.com>
2021-10-18 15:50:24 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -16,14 +16,14 @@ datas = soup.select(
     'div.contents > div.content01 > div > ul > li >article > div >h3'
     )
 
-data = []
+data = {}
 
 for title in datas:   
     name = title.find_all('a')[0].text
     url = 'http:'+title.find('a')['href']
     data[name] = url
 
-with open(os.path.join(BASE_DIR, 'news.json'), 'w+',encoding='utf-8') as json_file:
+with open(os.path.join(BASE_DIR, 'news1.json'), 'w+',encoding='utf-8') as json_file:
     json.dump(data, json_file, ensure_ascii = False, indent='\t')
 
 print('뉴스기사 스크래핑 끝')
