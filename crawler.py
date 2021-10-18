7d6ae25c03b7c0f9186f1542dffb72199b6d5830
wxysz <wxysz81@yahoo.com>
2021-10-18 16:13:49 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -17,10 +17,18 @@ rank2 = []
 rank1 = rank_json["data"]["league1"]
 rank2 = rank_json["data"]["league2"]
 rank_zip = rank1 + rank2
+
+print(rank_zip)
+
+'''
 for league_ranking in rank_zip:
     json.dumps(league_ranking, ensure_ascii=False, indent="\t")
     print(json.dumps(league_ranking, ensure_ascii=False, indent="\t"))
 
-with open(os.path.join(BASE_DIR, 'rank.json'), 'w+',encoding='utf-8') as make_file:
-    json.dump(league_ranking, make_file, ensure_ascii = False, indent='\t')
-    print(json.dump(league_ranking, make_file, ensure_ascii = False, indent='\t'))
+# with open(os.path.join(BASE_DIR, 'rank.json'), "r") as modify_json:
+#    rank_zip
+        
+with open(os.path.join(BASE_DIR, 'rank.json'), 'w+',encoding='utf-8') as write_file:
+    json.dump(league_ranking, write_file, ensure_ascii = False, indent='\t')
+    print(json.dump(league_ranking, write_file, ensure_ascii = False, indent='\t'))
+'''
