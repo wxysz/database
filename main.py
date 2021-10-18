43864c79b5834f3291f7e9bb4e64218382234502
wxysz <wxysz81@yahoo.com>
2021-10-19 08:57:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json_python
 import json
 
-json_dict = { 'category': 'food', 'name': 'mcdonalds', 'price': '1000' }
+json_db = { 'category': 'food', 'name': 'mcdonalds', 'price': '1000' }
 
-json_dict = json.loads(json.dumps(json_python.json_dict))
-print (json_dict)
+json_db = json.loads(json.dumps(main.json_db))
+print (json_db)
