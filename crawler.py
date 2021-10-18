cb97d725d96a30e1a089e5df9cf8b892b2b13770
wxysz <wxysz81@yahoo.com>
2021-10-18 15:42:45 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -18,8 +18,7 @@ url1 = 'https://kleague.com/api/clubRank.do'
 req1 = requests.get(url1)
 html1 = req1.content
 soup1 = BeautifulSoup(html1, 'html.parser')
-site_json=json.loads(soup1.content)
-
+rank_json=json.loads(soup1.text)
 
 datas = soup.select(
     'div.contents > div.content01 > div > ul > li >article > div >h3'
@@ -35,4 +34,15 @@ for title in datas:
 with open(os.path.join(BASE_DIR, 'news.json'), 'w+',encoding='utf-8') as json_file:
     json.dump(data, json_file, ensure_ascii = False, indent='\t')
 
+rank1 = []
+rank2 = []
+rank1 = rank_json["data"]["league1"]
+rank2 = rank_json["data"]["league2"]
+rank_zip = rank1 + rank2
+for league_ranking in rank_zip:
+    json.dump(league_ranking, ensure_ascii=False, indent="\t")
+
+with open(os.path.join(BASE_DIR, 'rank.json'), 'w+',encoding='utf-8') as make_file:
+    json.dump(rank_zip, make_file, ensure_ascii = False, indent='\t')
+    
 print('뉴스기사 스크래핑 끝')
