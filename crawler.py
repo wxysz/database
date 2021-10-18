12804df0b71539ebe0a21623a71fcc8053df3a50
wxysz <wxysz81@yahoo.com>
2021-10-18 15:35:29 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -8,8 +8,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 print('뉴스기사 스크래핑 시작')
 
-
-
 url = 'https://www.yna.co.kr/safe/news'
 req = requests.get(url)
 #req.encoding= None
@@ -20,13 +18,14 @@ url1 = 'https://kleague.com/api/clubRank.do'
 req1 = requests.get(url1)
 html1 = req1.content
 soup1 = BeautifulSoup(html1, 'html.parser')
-site_json=json.loads(soup1.text) # 추가
-                   
+site_json=json.loads(soup1.content)
+
+
 datas = soup.select(
     'div.contents > div.content01 > div > ul > li >article > div >h3'
     )
 
-data = {}
+data = []
 
 for title in datas:   
     name = title.find_all('a')[0].text
