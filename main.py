a0dcc4f124e060fe631da1efd17eada5721e6581
wxysz <wxysz81@yahoo.com>
2021-10-19 11:22:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ print('뉴스기사 스크래핑 시작')
 req = requests.get('https://www.yna.co.kr/safe/news')
 html = req.content
 soup = BeautifulSoup(html, 'html.parser')
-datas = soup.select(
+news_data = soup.select(
     'div.contents > div.content01 > div > ul > li >article > div >h3'
     )
 
 data = {}
 
-for title in datas:   
+for title in news_data:   
     name = title.find_all('a')[0].text
     url = 'http:'+title.find('a')['href']
     data[name] = url
 
 with open(os.path.join(BASE_DIR, 'news.json'), 'w+',encoding='utf-8') as json_file:
    reg = json.dump(data, json_file, ensure_ascii = False, indent='\t')
-   print(datas)
+   print(data)
     
 print('뉴스기사 스크래핑 끝')
 
