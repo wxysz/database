5455cbbd94eb456bf71731199274b3526fbf5552
wxysz <wxysz81@yahoo.com>
2021-10-19 10:24:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
+import requests
 import json
-'''
-json_dict = { 'category': 'food', 'name': 'mcdonalds', 'price': '1000' }
-add = { 'category': 'food', 'name': 'lotteria', 'price': '1500' }
+from bs4 import BeautifulSoup
+from github import Github
+from datetime import datetime
+from pytz import timezone
 
-json_dict = [json_dict, add]
-'''
-json_dict = {
-    'mcdonalds': {
-        'category': 'food',
-        'price': '1000'
-    }
-}
+seoul_timezone = timezone('Asia/Seoul')
+today = datetime.now(seoul_timezone)
+today_date = today.strftime("%Y년 %m월 %d일")
 
-add = {
-    'lotteria': {
-        'category': 'food',
-        'price': '1500'
-    }
-}
+url = 'https://kleague.com/api/clubRank.do'
+req = requests.get(url)
+html = req.content
+soup = BeautifulSoup(html, 'html.parser')
+rank_json=json.loads(soup.text)
+rank1 = []
+rank2 = []
+rank1 = rank_json["data"]["league1"]
+rank2 = rank_json["data"]["league2"]
+rank_zip = rank1 + rank2
+league_ranking = json.dumps(rank_zip, ensure_ascii=False, indent="\t")
 
-# json_dict.update(row)
-json_dict = [json_dict, add]
 
-json_dict = json.loads(json.dumps(json_dict, ensure_ascii=False, indent="\t"))
-print(json_dict)
+GITHUB_TOKEN = os.environ['MY_GITHUB_TOKEN']
+REPOSITORY_NAME = "database"
+repo = Github(GITHUB_TOKEN).get_user().get_repo(REPOSITORY_NAME)
+issue_title = f"K리그 순위 ({today_date})"
 
-with open('fileName.json', 'w') as outfile:
-    json.dump(json_dict, outfile, ensure_ascii=False, indent="\t")
+if issue_body != '' and REPO_NAME == repo.name:
+    reg = repo.create_issue(title=issue_title, body=league_ranking)
+    print(reg)
