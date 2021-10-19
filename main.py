2a2214051309c30390a6d1702e71650825e955b1
wxysz <wxysz81@yahoo.com>
2021-10-19 15:39:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,33 +23,16 @@ access_token = os.environ['MY_GITHUB_TOKEN']
 repository_name = "database" # 내 저장소 이름 필수로 바꿔야함 
 
 repo = Github(access_token).get_user().get_repo(repository_name)
-
+repo.create_file(rank_json, make_file, ensure_ascii = False, indent="\t")
 print(today_date)
+
 # print(rank_json)
-'''
-with open (os.path.join(BASE_DIR, 'rank.json'), "r", encoding="utf-8") as f:
-    reg = json.load(f)
-print(reg)
-'''
+
 with open(os.path.join(BASE_DIR, 'rank.json'), 'w+', encoding="utf-8") as make_file:
     reg = json.dump(rank_json, make_file, ensure_ascii = False, indent="\t")
 print(reg)
 
-json_object = {
-    "id": 1,
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874"
-    },
-    "admin": False,
-    "hobbies": None
-}
-
-with open(os.path.join(BASE_DIR, 'data.json'), 'w+') as f:
-    json_string = json.dump(json_object, f, indent=2)
-    print(json_string)
+with open (os.path.join(BASE_DIR, 'rank.json'), "r", encoding="utf-8") as f:
+    reg = json.load(f)
+print(reg)
 
