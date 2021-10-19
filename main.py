5e676efc56827e5b4dd3fe98dd2f52ac9935d82b
wxysz <wxysz81@yahoo.com>
2021-10-19 09:00:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
 
-json_dict = { 'category': 'food', 'name': 'mcdonalds', 'price': '1000' }
+col = { 'category': 'food', 'name': 'mcdonalds', 'price': '1000' }
+row= { 'category': 'food', 'name': 'lotteria', 'price': '1500' }
 
-
+json_dict = [col, row]
 
 json_db = json.loads(json.dumps(json_dict, ensure_ascii=False, indent="\t"))
 print (json_db)
