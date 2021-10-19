087e1d811028052de9fe6d6d05281cd27d55426e
wxysz <wxysz81@yahoo.com>
2021-10-19 09:19:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import json
 
 json_dict = { 'category': 'food', 'name': 'mcdonalds', 'price': '1000' }
-update = { 'category': 'food', 'name': 'lotteria', 'price': '1500' }
+add = { 'category': 'food', 'name': 'lotteria', 'price': '1500' }
 
-json_dict = [json_dict, update]
+json_dict = [json_dict, add]
+
+json_dict.update(add)
 
 json_dict = json.loads(json.dumps(json_dict, ensure_ascii=False, indent="\t"))
 print (json_dict)
