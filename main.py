7ea3f3ceae5095a9072c2f43d9ac5e95ad3b8d6c
wxysz <wxysz81@yahoo.com>
2021-10-19 11:17:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import os
-import re
 import requests
 import json
 from bs4 import BeautifulSoup
 from github import Github
 from datetime import datetime
 from pytz import timezone
-from urllib.request import urlopen
-from html import unescape
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+print('뉴스기사 스크래핑 시작')
+
+req = requests.get('https://www.yna.co.kr/safe/news')
+req.encoding= None
+html = req.content
+soup = BeautifulSoup(html, 'html.parser')
+datas = soup.select(
+    'div.contents > div.content01 > div > ul > li >article > div >h3'
+    )
+
+data = {}
+
+for title in datas:   
+    name = title.find_all('a')[0].text
+    url = 'http:'+title.find('a')['href']
+    data[name] = url
+
+with open(os.path.join(BASE_DIR, 'news.json'), 'w+',encoding='utf-8') as json_file:
+    json.dump(data, json_file, ensure_ascii = False, indent='\t')
+    
+print('뉴스기사 스크래핑 끝')
+
+'''
 seoul_timezone = timezone('Asia/Seoul')
 today = datetime.now(seoul_timezone)
 today_date = today.strftime("%Y년 %m월 %d일")
@@ -38,3 +60,4 @@ with open('rank.json', 'w+',encoding='utf-8') as write_file:
     reg = json.dump(league_ranking, write_file, ensure_ascii = False, indent='\t')
     
     print(reg)
+'''
