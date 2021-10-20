4ce1fba31ca9362e31abf44f5bad21ea23062d9d
wxysz <wxysz81@yahoo.com>
2021-10-20 12:55:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,10 +18,6 @@ req = requests.get(url)
 soup = BeautifulSoup(req.content, 'html.parser')
 rank = json.loads(soup.text)
 
-contents = ''
-
-contents += rank
-
 access_token = os.environ['MY_GITHUB_TOKEN']
 repository_name = "database" # 내 저장소 이름 필수로 바꿔야함 
 
@@ -39,10 +35,63 @@ with open(os.path.join(BASE_DIR, './rank.json'), 'w+', encoding="utf-8") as make
     reg = json.dump(rank, make_file, ensure_ascii = False, indent="\t")
 print(reg)
 
+s = "국어,수학,정보,영어,체육"
+
+new_lst = s.split(",")   
+
+print(new_lst) 
+
+
 '''
 with open (os.path.join(BASE_DIR, 'rank.json'), "r", encoding="utf-8") as f:
     reg = json.load(f)
 print(reg)
 '''
 
+'''
+if __name__ == "__main__":
+    access_token = os.environ['MY_GITHUB_TOKEN']
+    repository_name = "mffdb.github.io" # 내 저장소 이름 필수로 바꿔야함 
 
+    seoul_timezone = timezone('Asia/Seoul')
+    today = datetime.now(seoul_timezone)
+    today_date = today.strftime("%Y년 %m월 %d일")
+
+    yes24_it_new_product_url = "http://www.yes24.com/24/Category/NewProductList/001001003?sumGb=01"
+
+#    soup = parsing_beautifulsoup(yes24_it_new_product_url)
+
+    data = requests.get(yes24_it_new_product_url)
+    html = data.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+#    upload_contents = extract_book_data(soup)
+
+    upload_contents = ''
+    new_books = soup.select(".goodsTxtInfo")	# 새 함수 선언 예스24 사이트 goodsTxtInfo 클래스 안에 내용
+    url_prefix = "http://www.yes24.com"
+
+    for new_book in new_books:
+        book_name = new_book.select("a")[0].text
+        url_suffix = new_book.select("a")[1].attrs['href']
+        url = url_prefix + url_suffix
+        price = new_book.select(".priceB")[0].text
+
+        content = f"<a href={url}>" + book_name + "</a>" + ", " + price + "<br/>\n"
+        upload_contents += content
+
+#    repo = get_github_repo(access_token, repository_name)
+
+    g = Github(access_token)
+    repo = g.get_user().get_repo(repository_name)
+
+    issue_title = f"YES24 IT 신간 도서 알림({today_date})"
+
+    
+#    upload_github_issue(repo, issue_title, upload_contents)
+
+    repo.create_issue(title=issue_title, body=upload_contents)
+    #repo.create_file("/rank.json", "commit message", upload_contents)
+       
+    print("Upload Github Issue Success!")
+'''
