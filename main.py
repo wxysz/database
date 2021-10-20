1be2f165fd10fcf031c25bd62fa0f8f4e5f0c05f
wxysz <wxysz81@yahoo.com>
2021-10-20 10:18:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ today_date = today.strftime("%Y년 %m월 %d일")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-rank = ''
-
 url = 'https://kleague.com/api/clubRank.do'
 req = requests.get(url)
 soup = BeautifulSoup(req.content, 'html.parser')
@@ -24,7 +22,8 @@ access_token = os.environ['MY_GITHUB_TOKEN']
 repository_name = "database" # 내 저장소 이름 필수로 바꿔야함 
 
 repo = Github(access_token).get_user().get_repo(repository_name)
-repo.create_file("rank.json", "commit message", rank)
+
+repo.create_file("rank.json", "commit message", "dsfsdfsdfsfd")
 
 print(today_date)
 
