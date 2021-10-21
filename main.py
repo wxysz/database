73eeb093e7126e44f199383b65cd5545d1aa9616
wxysz <wxysz81@yahoo.com>
2021-10-21 10:23:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,14 +25,15 @@ repo = Github(access_token).get_user().get_repo(repository_name)
 
 issue_title = f"리그 순위표({today_date})"
 
+print(today)
 print(today_date)
 
 with open(os.path.join(BASE_DIR, 'rank.json'), 'w+', encoding="utf-8") as make_file:
     reg = json.dump(rank, make_file, ensure_ascii = False, indent="\t")
 
 rank_json = json.dumps(rank, sort_keys=True)  # dict()를 str로 변경 rank, indent=2, sort_keys=True
-repo.create_issue(title=issue_title, body=rank_json)  # 실행가능 
-repo.create_file('rank.json', "commit message", rank_json) # 실행가능
+# repo.create_issue(title=issue_title, body=rank_json)  # 실행가능 
+# repo.create_file('rank.json', "commit message", rank_json) # 실행가능
 
 '''
 with open (os.path.join(BASE_DIR, 'rank.json'), "r", encoding="utf-8") as f:
