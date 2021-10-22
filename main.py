821778f7e3c8df97ef41e91dc477f1fc063253ef
wxysz <wxysz81@yahoo.com>
2021-10-22 16:21:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,14 +24,13 @@ repository_name = "database" # 내 저장소 이름 필수로 바꿔야함
 
 repo = Github(access_token).get_user().get_repo(repository_name)
 
+issues1 = repo.get_issues(state='open')	# 저장소의 이슈를 받아와서 상태를 열기
+for issue1 in issues1:
+    if "날짜 발열 테스트" in issue1.title:	# 저장소 제목이 날짜 발열 테스트 라면 
+        issue1.edit(state='closed')	# 이슈를 에디트 해서 상태를 닫기
+        print(issue1.title)	# 이슈 제목을 프린트
+		
 issue_title = f"리그 순위표({today_date})"
-
-issues = repo.get_issues(state='open')	# 저장소의 이슈를 받아와서 상태를 열기
-    for issue in issues:
-        if "날짜 발열 테스트" in issue.title:	# 저장소 제목이 날짜 발열 테스트 라면 
-            issue.edit(state='closed')	# 이슈를 에디트 해서 상태를 닫기
-
-print(f"이슈 제목({issue.title}))	# 이슈 제목을 프린트
 		
 print(today)
 print(today_date)
