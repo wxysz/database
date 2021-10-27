42563a74a341f6d19cd91a8987a3dca219c7002f
wxysz <wxysz81@yahoo.com>
2021-10-27 16:04:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ print(f"-----------------------리그 순위표({today})-----------------------"
 ##########################################
 
 for i in rank['data']['league1']:
-temp = i.replace(["teamName"], ["구단"])
+temp = i.replace(rank['data']['league1'][0]["teamName"], rank['data']['league1'][0]["구단"])
 result.append(temp)
 
 print(result)
