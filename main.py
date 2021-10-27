e455c343cae6afec56c0f6d9e2b1cd4f0d898a16
wxysz <wxysz81@yahoo.com>
2021-10-27 14:05:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -42,27 +42,8 @@ for issue1 in issues1:
 print(f"-----------------------리그 순위표({today})-----------------------")
 
 ##########################################
-
-l = ['Alice', 'Bob', 'Charlie', 'Bob', 'Dave']
-
-l.remove('Alice', 'Bob')
-print(l)
-
-
-'''
 print(f"-------------------------------------------------------------------------------------------")
 
-# league1 = rank['data']['league1']['rank'] + 
-rank['data']['league1']['teamName'] + 
-rank['data']['league1']['gameCount'] + 
-rank['data']['league1']['gainPoint'] + 
-rank['data']['league1']['winCnt'] + 
-rank['data']['league1']['tieCnt'] + 
-rank['data']['league1']['lossCnt'] + 
-rank['data']['league1']['gainGoal'] + 
-rank['data']['league1']['lossGoal'] + 
-rank['data']['league1']['gapCnt']
-# rank_league1 = DataFrame(league1)
 league1 = DataFrame(rank['data']['league1'])
 display(league1)
 
@@ -72,10 +53,7 @@ league2 = DataFrame(rank['data']['league2'])
 display(league2)
 
 print(f"-------------------------------------------------------------------------------------------")
-'''
 #############################################
-
-
 
 # repo.create_issue(title=issue_title, body=rank_json)  # 실행가능 
 # repo.create_file('rank.json', "commit message", rank_json) # 실행가능
