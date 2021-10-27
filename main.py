518ef88b61acc66cea65200855c3c9454e490f08
wxysz <wxysz81@yahoo.com>
2021-10-27 15:32:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -42,29 +42,38 @@ for issue1 in issues1:
 print(f"-----------------------리그 순위표({today})-----------------------")
 
 ##########################################
-del rank['data']['league1'][0]['year']
-del rank['data']['league1'][0]['leagueId']
-del rank['data']['league1'][0]['teamId']
-del rank['data']['league1'][0]['winNqty']
-del rank['data']['league1'][0]['winEqty']
-del rank['data']['league1'][0]['winTKqty']
-del rank['data']['league1'][0]['tieCnt']
-del rank['data']['league1'][0]['lossCnt']
-del rank['data']['league1'][0]['lossGoal']
-del rank['data']['league1'][0]['homepage']
-del rank['data']['league1'][0]['lang']
-del rank['data']['league1'][0]['stadium']
-del rank['data']['league1'][0]['recordType']
-del rank['data']['league1'][0]['assignGameType']
-del rank['data']['league1'][0]['game01']
-del rank['data']['league1'][0]['game02']
-del rank['data']['league1'][0]['game03']
-del rank['data']['league1'][0]['game04']
-del rank['data']['league1'][0]['game05']
-del rank['data']['league1'][0]['game06']
+
+for i range(0,11) :
+del rank['data']['league1'][i]['year']	# 년도
+del rank['data']['league1'][i]['leagueId']	# 리그 아이디
+del rank['data']['league1'][i]['teamId']	# 팀 아이디
+# del rank['data']['league1'][i]['teamName']	# 팀 이름
+# del rank['data']['league1'][i]['rank']	# 팀 순위
+# del rank['data']['league1'][i]['gainPoint']	# 승점
+del rank['data']['league1'][i]['winCnt']	# 승
+del rank['data']['league1'][i]['winNqty']	# 모름
+del rank['data']['league1'][i]['winEqty']	# 모름
+del rank['data']['league1'][i]['winTKqty']	# 모름
+del rank['data']['league1'][i]['tieCnt']	# 무
+del rank['data']['league1'][i]['lossCnt']	# 패
+del rank['data']['league1'][i]['gapCnt']	# 득실
+# del rank['data']['league1'][i]['gainGoal']	# 득점
+del rank['data']['league1'][i]['lossGoal']	# 실점
+# del rank['data']['league1'][i]['gameCount']	# 경기수
+del rank['data']['league1'][i]['homepage']	# 홈페이지
+del rank['data']['league1'][i]['lang']	# 언어
+del rank['data']['league1'][i]['stadium']	# 경기장
+del rank['data']['league1'][i]['recordType']	# 타입
+del rank['data']['league1'][i]['assignGameType']	# 모름
+del rank['data']['league1'][i]['game01']	# 첫
+del rank['data']['league1'][i]['game02']	# 둘
+del rank['data']['league1'][i]['game03']	# 셋	
+del rank['data']['league1'][i]['game04']	# 넷
+del rank['data']['league1'][i]['game05']	# 다
+del rank['data']['league1'][i]['game06']	# 여
+
 
 league1 = DataFrame(rank['data']['league1'])
-
 display(league1)
 
 
