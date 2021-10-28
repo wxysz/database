33334696f7895ed45892f8e51971b63cdde45156
wxysz <wxysz81@yahoo.com>
2021-10-28 11:56:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -34,12 +34,14 @@ repo = Github(access_token).get_user().get_repo(repository_name)
 
 issue_title = f"저장 시간({today_date})"
 
+file = repo.get_contents("rank.json")
+
 issues1 = repo.get_issues(state='open')	# 저장소의 이슈를 받아와서 상태를 열기
 for issue1 in issues1:
     if "날짜 발열 테스트" in issue1.title:	# 저장소 제목이 날짜 발열 테스트 라면 
         issue1.edit(state='closed')	# 이슈를 에디트 해서 상태를 닫기
 
-# file = repo.get_contents("rank.json")
+# 
 	
 # repo.delete_file('rank.json', "commit message", file.sha ) # 실행가능
 # repo.update_file('rank.json', "commit message", rank_json, file.sha)
