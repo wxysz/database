955028eba435a1f6c79ca1512cab5066ba57679d
wxysz <wxysz81@yahoo.com>
2021-10-28 14:51:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ repo = Github(access_token).get_user().get_repo(repository_name)
 issue_title = f"저장 시간({today_date})"
 issue_body = f"저장 시간({today_date})"
 
-file = repo.get_contents('rank.json', ref="rank")
+file = repo.get_contents('rank.json', ref=rank)
 
 repo.create_issue(title=issue_title, body=issue_body)
 
